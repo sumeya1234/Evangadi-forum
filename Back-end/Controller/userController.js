684df1b783269c913f913d990edaa4f6e515d243
sumeya1234 <sumsum8336@gmail.com>
2024-10-02 08:09:00 -0700

    Password encryption for register function

--- Back-end/Controller/userController.js
+++ Back-end/Controller/userController.js
@@ -1,22 +1,31 @@
-const dbConnection = require("../DB/dbConfig")
-const {StatusCodes} = require("http-status-codes")
+const dbConnection = require("../DB/dbConfig");
+const {StatusCodes} = require("http-status-codes");
+const bcrypt = require("bcrypt");
 async function register(req,res){
     const { username, firstname, lastname, email, password } = req.body;
-    if (!username || !firstname || !lastname || !email || !password ){
+    if ( !username || !firstname || !lastname || !email || !password ){
         return res
             .status(StatusCodes.BAD_REQUEST)
             .json({msg : "Please provide all required fields"})
     }
     try {
         const [user] = await dbConnection.query("SELECT username, userid FROM users WHERE username = ? or email = ?" , [username, email])
-        // return res.json({user : user});
+        // console.log(user[0].userid); 
+        // console.log({user : });
         if (user.length > 0) {
             return res.status(StatusCodes.BAD_REQUEST)
             .json({ msg : "You have already registered"});
         }
+        if (password.length <= 8){
+            return res.status(StatusCodes.BAD_REQUEST)
+            .json({ msg : "Password must be at least 8 characters"});
+        }
+        // Password encryption
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
         await dbConnection.query(
             "INSERT INTO users (username,firstname, lastname, email, password) VALUES (?,?,?,?,?)",
-            [username,firstname,lastname,email,password]
+            [username,firstname,lastname,email,hashedPassword]
         );
         return res.status(StatusCodes.CREATED).json({msg : "User created successfully"});
     } catch (error) {
